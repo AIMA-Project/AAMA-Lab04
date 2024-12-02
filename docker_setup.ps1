9e0c1e623078f00d9cffd6f4cc12c4ef564ff473
Anna <wheelercs@protonmail.com>
2024-12-02 13:29:40 -0600

    Fixed Minor Issues
    Missing `.gitignore` and reference venv.
    
    Updated bad link to Docker Hub.
    
    Merged Linux script into Windows PowerShell.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

--- docker_setup.ps1
+++ docker_setup.ps1
@@ -1,3 +1,4 @@
+#!/bin/bash
 
-docker build -t lab04 .
-docker run --name lab4 -p 8888:8888 lab04
+docker build -t "lab4-container" .
+docker run --name "lab4-container" -p 8888:8888 lab04
--- docker_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t lab04 .
-sudo docker run --name lab4 -p 8888:8888 lab04
